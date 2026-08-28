42d6bc85987e364430fc0a665fc7368d82ebff08
CALI RH <patricia@calirh.com>
2026-08-28 19:26:50 -0300

    style: load live workspace state styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from './App';
 import './styles.css';
 import './modules.css';
 import './ux-v2.css';
+import './runtime.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
