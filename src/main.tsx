2d4c9a0e5552fddeb61e0b05cf5019224d4b8145
CALI RH <patricia@calirh.com>
2026-08-28 21:33:42 -0300

    style: load workspace invariants after page refinements

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './page1.css';
 import './page1-hotfix.css';
 import './page1-pass2.css';
 import './page1-rules.css';
+import './workspace-invariants.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
