3d58d9e4b024c8f327be5a7cdd6db39cfa4e8264
CALI RH <patricia@calirh.com>
2026-09-11 11:34:46 -0300

    Load project styles with client deliverables route

--- src/pages/client/ClientDeliverablesPage.tsx
+++ src/pages/client/ClientDeliverablesPage.tsx
@@ -5,6 +5,14 @@ import {
   MessageCircle, RefreshCw, Send, Star, X,
 } from 'lucide-react';
 import { Shell } from '../../components/WorkspaceShell';
+import '../../page5-projects.css';
+import '../../page5-projects-flow-v2.css';
+import '../../page5-projects-polish-v3.css';
+import '../../projects-planning-intelligence-v36.css';
+import '../../project-approval-workflow-v38.css';
+import '../../project-approval-rules-v39.css';
+import '../../project-client-portfolio-v39.css';
+
 import { demoDeliverables } from '../../data/demo';
 import {
   loadClientDeliveryReality,
