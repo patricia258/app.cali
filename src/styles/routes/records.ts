887a950225d008d8ccda8137ff33fe59fa9588c9
CALI RH <patricia@calirh.com>
2026-09-16 13:41:55 -0300

    Agrupa estilos de ocorrencias por rota

---
+++ src/styles/routes/records.ts
@@ -0,0 +1,9 @@
+import '../../records-v12.css';
+import '../../records-v13-responsive-fix.css';
+import '../../records-v14-overlay-fix.css';
+import '../../records-v20-conversation-polish.css';
+import '../../records-v21-chat-theme-fix.css';
+import '../../records-v22-message-controls.css';
+import '../../records-v25-operations.css';
+
+export {};
