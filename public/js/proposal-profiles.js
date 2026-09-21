1cf5a82d826151da79822872a0f65e996cafa6f3
CALI RH <patricia@calirh.com>
2026-09-21 14:50:30 -0300

    Sincroniza proposta com briefing de treinamentos

--- public/js/proposal-profiles.js
+++ public/js/proposal-profiles.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@3bf5456fc582d03abfda619887f4b3dade44bec8/js/proposal-profiles.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@2ca96e0e455ca7b9d9fa87c59526d0c0849fa4d6/js/proposal-profiles.js";
