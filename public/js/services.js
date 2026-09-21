1d683fc7d8e00511803d6eca20e823e0bf05e7ab
CALI RH <patricia@calirh.com>
2026-09-21 15:22:25 -0300

    Sincroniza preview com regras de treinamentos

--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@43f9165e6f0882313854c58e2c5f8a0b0d3f205d/js/services.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@bfddac1d070e5af8784b7490cfa9e7d35fb25e8d/js/services.js";
