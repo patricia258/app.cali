5ce5859950875c6fe81e80a15770892c4e77cc87
CALI RH <patricia@calirh.com>
2026-09-21 14:58:02 -0300

    Aponta PDF para commit produtivo do Portal
    * Aponta proposta para o commit produtivo do portal
    
    * Aponta proposta para o commit produtivo do portal

--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@8b7f905f11a8dd97cdbd0681896046061b5853e4/js/services.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@43f9165e6f0882313854c58e2c5f8a0b0d3f205d/js/services.js";
