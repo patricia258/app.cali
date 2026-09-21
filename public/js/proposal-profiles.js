99c110875f4d8eef41a449e2c80f113694846ecd
CALI RH <patricia@calirh.com>
2026-09-21 19:58:20 -0300

    Finaliza proposta e logística de Treinamentos
    * Alinha regras de logística de treinamentos no app
    
    * Adiciona logística editável às propostas de treinamento
    
    * Sincroniza regras do Portal no Workspace
    
    * Sincroniza regras do Portal no Workspace
    
    * Atualiza prévia para o novo PDF de treinamentos
    
    * Atualiza nomenclatura do formato remoto
    
    * Ajusta editor ao fluxo final de treinamentos
    
    * Sincroniza preview com o Portal final
    
    * Sincroniza preview com o Portal final
    
    * Sincroniza preview com o Portal final

--- public/js/proposal-profiles.js
+++ public/js/proposal-profiles.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@d754b19102695611c0aa95aa202b071fbb792b57/js/proposal-profiles.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@bf2db3ead544fd40f8a6cb97fbcd2f7c29ad7d4c/js/proposal-profiles.js";
