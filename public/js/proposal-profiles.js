4636699267d800b1f14f3ca64921ef5ddb1d113b
CALI RH <patricia@calirh.com>
2026-09-21 14:54:20 -0300

    Atualiza referência do portal de treinamentos

--- public/js/proposal-profiles.js
+++ public/js/proposal-profiles.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@2ca96e0e455ca7b9d9fa87c59526d0c0849fa4d6/js/proposal-profiles.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@8b7f905f11a8dd97cdbd0681896046061b5853e4/js/proposal-profiles.js";
