7da973c7a524fcbe417626e7563df11faa869863
CALI RH <patricia@calirh.com>
2026-09-21 19:53:53 -0300

    Sincroniza regras do Portal no Workspace

--- public/js/services.js
+++ public/js/services.js
@@ -1,1 +1,1 @@
-export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@d754b19102695611c0aa95aa202b071fbb792b57/js/services.js";
+export * from "https://cdn.jsdelivr.net/gh/patricia258/cali-portal@f1edcd72f5a8dfa780ab67ae784664de98013022/js/services.js";
